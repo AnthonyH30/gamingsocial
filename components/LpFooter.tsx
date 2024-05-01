2df1ef4a5757e45b483f36b227bc87ebc30ca44d
AnthonyH30 <anthonyharisson30@gmail.com>
2024-05-01 19:02:11 -0300

    feat: Adding landing page content - mock infos

--- components/LpFooter.tsx
+++ components/LpFooter.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 export default function LpFooter() {
   return (
     <footer className='bg-black text-xs flex flex-col md:flex-row items-center justify-center md:justify-between text-white min-h-20 border-t border-white gap-4 py-6 md:px-24'>
-        <p>© 2024 All Rights Reserved</p>
+        <p className='hover:text-red-600 ease-in duration-200 cursor-pointer'>© 2024 All Rights Reserved</p>
         <ul className='flex flex-col md:flex-row gap-4'>
-            <li>Privacy Policy</li>
-            <li>Terms of Use</li>
-            <li>Sales and Refunds</li>
-            <li>Legal</li>
-            <li>Site Map</li>
+            <li className='hover:text-red-600 ease-in duration-200 cursor-pointer'>Privacy Policy</li>
+            <li className='hover:text-red-600 ease-in duration-200 cursor-pointer'>Terms of Use</li>
+            <li className='hover:text-red-600 ease-in duration-200 cursor-pointer'>Sales and Refunds</li>
+            <li className='hover:text-red-600 ease-in duration-200 cursor-pointer'>Legal</li>
+            <li className='hover:text-red-600 ease-in duration-200 cursor-pointer'>Site Map</li>
         </ul>
     </footer>
   )
